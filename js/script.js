67fe8118da1c187d4d6b798ea409b1fb41beaa21
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-10 15:03:55 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert("ciao");
+var firstName = prompt("inserisci il tuo nome");
+// console.log(firstName);
+
+var lastName = prompt("Inserisci il tuo cognome");
+// console.log(lastName);
+
+var favouriteColor = prompt("Inserisci il tuo colore preferito");
+// console.log(favouriteColor);
+
+var currentYear = 20
+// console.log(currentYear);
+
+var currentYearString = currentYear.toString();
+// console.log(currentYearString);
+
+// Soluzione HTML
+document.getElementById("first-name").innerHTML = firstName;
+document.getElementById("last-name").innerHTML = lastName;
+document.getElementById("favourite-color").innerHTML = favouriteColor;
+document.getElementById("current-year").innerHTML = currentYearString;
+// Soluzione HTML
+
+
+// Soluzione JS
+document.writeln("La tua password è: " + firstName + lastName + favouriteColor + currentYear);
+// Soluzione JS
